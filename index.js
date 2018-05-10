755970ef31c85eb4db6ee2b1c437d286e2c7eb74
Oleg <oleg.r646@gmail.com>
2018-05-10 19:17:31 +0300

    test commit to pull from rep

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 //TODO Ездящие туда-сюда машины на заднем плане
 
-let  list = document.getElementById("test");
-let allowMapApiUse = false;
-list.addEventListener("click", clicked);
+var brandsList;
+var modelsList;
+var visibleModelsList;
 
-tester();
 
-function clicked(event){
-    console.log(list.value);
-}
-function mapApiLoaded(){
-    console.log(456);
-    allowMapApiUse = true;
-}
+
+
+
+
+
+
+
+
 //AIzaSyC-oP-CtwxnCTbpAEekGevXEujfbvtUYMQ
